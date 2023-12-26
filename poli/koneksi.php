6b6ae2c042e9ae9091afdedf618c8fd66e906304
Fabianus Jericho HJS <fabianusjeriko@gmail.com>
2023-12-26 23:28:45 +0700

    first commit

---
+++ poli/koneksi.php
@@ -0,0 +1,13 @@
+<?php 
+$databaseHost = 'localhost:3308';
+$databaseName = 'poli';
+$databaseUsername = 'root';
+$databasePassword = '';
+ 
+$mysqli = mysqli_connect($databaseHost, 
+    $databaseUsername, $databasePassword, $databaseName);
+
+// Periksa koneksi
+if (!$mysqli) {
+    die("Koneksi gagal: " . mysqli_connect_error());
+}
